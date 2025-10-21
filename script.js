39c4d73bad7781f8cdeec8cc6fad647225b3e0f1
Aman <82828343+AmanTheDisciple@users.noreply.github.com>
2025-10-21 11:19:48 +0100

    final commit

--- script.js
+++ script.js
@@ -1,40 +1,107 @@
-function getComputerChoice(){
-    let randomNumber=Math.random()*10;
-    if(randomNumber<=3){
-        return "rock";
-    }else if(randomNumber<=6){
-        return "paper";
-    }else{
-        return "scissor";
-    }
-}
 
-function getHumanChoice(){
-    return prompt('Enter rock, paper or scissor');
+function playGame(){
+
+    let humanScore=0;
+    let computerScore=0;
     
-}
-
-let humanScore=0;
-let computerScore=0;
-
-let computerChoice=getComputerChoice();
-let humanChoice=getHumanChoice();
-function playRound(humanChoice,computerChoice,humanScore,computerScore){
-    humanChoice=humanChoice.toLowerCase();
-    console.log(`Computer: ${computerChoice}!
-                 You: ${humanChoice}!);
-    if(humanChoice=="scissor"){
-        switch(computerChoice){
-            case "paper":
-                console.log(`Computer: ${computerChoice}!
-                             You: ${humanChoice}!
-                             You Win!`);
-                humanScore++;
-                return;
-            case "scissor":
-                console.log(`Computer: ${computerChoice}!
-                             You: ${humanChoice}!
-                             Tie!`);
+    function getComputerChoice(){
+        let randomNumber=Math.random()*10;
+        if(randomNumber<=3){
+            return "rock";
+        }else if(randomNumber<=6){
+            return "paper";
+        }else{
+            return "scissor";
         }
     }
+
+    function getHumanChoice(){
+        return prompt('Enter rock, paper or scissor');
+    }
+
+    function playRound(humanChoice,computerChoice){
+        humanChoice=humanChoice.toLowerCase();
+        console.log(`Computer: ${computerChoice}!
+You: ${humanChoice}!`);
+        switch(humanChoice){
+            case "scissor":
+                switch(computerChoice){
+                    case "paper":
+                    return 1;
+                case "scissor":
+                    return 0;
+                case "rock":
+                    return -1;
+                }
+            case "rock":
+                switch(computerChoice){
+                    case "paper":                
+                        return 1;
+                    case "rock":
+                        return 0;
+                    case "scissor":
+                        return -1;
+                }
+            case "paper":
+                switch(computerChoice){
+                    case "rock":
+                        return 1;
+                    case "paper":
+                        return 0;
+                    case "scissor":
+                        return -1;
+                }
+            case "scissor":
+                switch(computerChoice){
+                    case "paper":
+                        return 1;
+                    case "scissor":
+                        return 0;
+                    case "rock":
+                        return -1;
+                    }
+            }       
+    }
+
+    let computerChoice;
+    let humanChoice;
+
+    for(let i=0;i<5;i++){
+        console.log(`Round ${i}`);
+        console.log(`Score
+                     You - Computer
+                     ${humanScore} | ${computerScore}`);
+        computerChoice=getComputerChoice();
+        humanChoice=getHumanChoice();
+        let outcome=playRound(humanChoice,computerChoice);
+        if(outcome==1){
+            console.log("You Win!");
+            humanScore++;
+        }else if(outcome==0){
+            console.log("Tie!");
+        }else{
+            console.log('You Lose!');
+            computerScore++;
+        }
+    }
+
+    if(humanScore>computerScore){
+        console.log(`You are the ultimate winner!
+                     Score
+                     You - Computer
+                     ${humanScore} | ${computerScore}`);
+    }else if(humanScore<computerScore){
+        console.log(`You have lost this game
+                     Score
+                     You - Computer
+                     ${humanScore} | ${computerScore}`);
+    }else{
+        console.log(`It was a tie
+                     Score
+                     You - Computer
+                     ${humanScore} | ${computerScore}`);
+    }
 }
+
+playGame();
+
